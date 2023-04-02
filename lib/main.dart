931874fb6adee9da71bde6705f39058a3c05c32a
satoshi <uchiyama.s@timeshappy.com>
2023-04-02 13:42:05 +0900

    リリース完了 ver2.0 -3

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:healthcare_mania_legacy_new/screens/model_list_screen.dart';
 //import 'package:flutter_stetho/flutter_stetho.dart';
+//classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
 
 void main() async {runApp( MyApp());
 }
