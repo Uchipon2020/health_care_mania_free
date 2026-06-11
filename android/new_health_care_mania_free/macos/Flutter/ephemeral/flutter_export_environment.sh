50ad62b2f5f1b7c94a8b4679aa2c674c13198f8d
Uchiyama Satoshi <60647880+Uchipon2020@users.noreply.github.com>
2026-06-11 23:21:19 +0900

    指定の修正と、細部の調整

---
+++ android/new_health_care_mania_free/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/satoshi/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/satoshi/StudioProjects/health_care_mania_free/android/new_health_care_mania_free"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
